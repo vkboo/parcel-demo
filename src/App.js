144168741fb1fd439aa86a8ab26b101e03f94e74
bo.zhao <bo.zhao@iglooinsure.com>
2024-05-23 18:00:05 +0800

    feat: support tailwindcss

---
+++ src/App.js
@@ -0,0 +1,3 @@
+export function App() {
+    return <h1 className="text-red-200">Hello world!</h1>;
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
+import "./tailwind.init.css";
 
 const container = document.getElementById("app");
 const root = createRoot(container)
